e5bec027c2d7d3c41a187ca5bcc2bcdcb255622e
Zverina1337 <danii.zvorugin@gmail.com>
2026-05-01 17:22:11 +0700

    fix: deleteProject bug

--- src/entities/projects/model/project.ts
+++ src/entities/projects/model/project.ts
@@ -6,29 +6,47 @@ import { ProjectService } from "./service";
 export const useProjectStore = defineStore("project", () => {
   const projects = ref<IProject[]>([]);
 
-  const fetchProjects: IProjectActions["fetchProjects"] = async () => {
-    const result = await ProjectService.getProjects();
-    projects.value = [...result];
-  };
+  const fetchProjects: IProjectActions["fetchProjects"] =
+    async () => {
+      const result = await ProjectService.getProjects();
+      projects.value = [...result];
+    };
 
-  const createProject: IProjectActions["createProject"] = async (data) => {
+  const createProject: IProjectActions["createProject"] = async (
+    data,
+  ) => {
     const result = await ProjectService.createProject(data);
     if (!result) return;
     projects.value = [...projects.value, result];
   };
 
-  const updateProject: IProjectActions["updateProject"] = async (uuid, data) => {
+  const updateProject: IProjectActions["updateProject"] = async (
+    uuid,
+    data,
+  ) => {
     const result = await ProjectService.updateProject(uuid, data);
     if (!result) return;
-    const foundedProject = projects.value.findIndex((project) => project.uuid === result.uuid);
+    const foundedProject = projects.value.findIndex(
+      (project) => project.uuid === result.uuid,
+    );
     projects.value[foundedProject] = result;
   };
 
-  const deleteProject: IProjectActions["deleteProject"] = async (uuid) => {
+  const deleteProject: IProjectActions["deleteProject"] = async (
+    uuid,
+  ) => {
     const result = await ProjectService.deleteProject(uuid);
     if (result !== null) return;
-    projects.value = projects.value.filter((project) => project.uuid === uuid);
+    projects.value = projects.value.filter(
+      (project) => project.uuid !== uuid,
+    );
   };
 
-  return { projects, fetchProjects, createProject, updateProject, deleteProject };
+  return {
+    projects,
+    fetchProjects,
+    createProject,
+    updateProject,
+    deleteProject,
+  };
 });
